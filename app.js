fa51bb48dd38a4bb09bee52d3cbd5a4999ff8c38
itsathefak <athefayubkhan@gmail.com>
2024-08-21 10:46:09 -0400

    Setting up Promises - 1

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // Call Back hell
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function colorChange(color, delay, nextColorChange) {
-  setTimeout(() => {
-    h1.style.color = color;
-    if (nextColorChange) nextColorChange();
-  }, delay);
+// function colorChange(color, delay, nextColorChange) {
+//   setTimeout(() => {
+//     h1.style.color = color;
+//     if (nextColorChange) nextColorChange();
+//   }, delay);
+// }
+
+// colorChange("red", 1000, () => {
+//   colorChange("blue", 1000, () => {
+//     colorChange("green", 1000);
+//   });
+// });
+
+// Setting up promises
+
+function savetoDB(data) {
+  let internetSpeed = Math.floor(Math.random() * 10) + 1;
+  if (internetSpeed > 4) {
+    console.log(`Your data has been stored : ${data}`);
+  } else {
+    console.log("Weak Connection, Data Not stored !");
+  }
 }
-
-colorChange("red", 1000, () => {
-  colorChange("blue", 1000, () => {
-    colorChange("green", 1000);
-  });
-});
