f8347ad05ffdb3730ef222296b86aadf8a3544bf
itsathefak <athefayubkhan@gmail.com>
2024-08-20 22:08:05 -0400

    JS is Single Threaded

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-function one() {
-  return 1;
-}
-function two() {
-  return one() + one();
-}
+setTimeout(() => {
+  console.log("My Name is Athef");
+}, 2000);
 
-function three() {
-  let ans = two() + one();
-  console.log(ans);
-}
-three();
+setTimeout(() => {
+  console.log("I'm a CS Student");
+}, 2000);
+
+console.log("Hello...");
+
+// Js is Single Threaded - You might see JS executes 2 setTimeout at the same time, thats possible cuz settimeout is run by browser (c++) and not JS, JS executes remaining code while browser stores the function in call stack.
