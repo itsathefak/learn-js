05758ee278d8c00664995762a5fcee4d2a2a705b
itsathefak <athefayubkhan@gmail.com>
2024-08-22 12:08:14 -0400

    Promises

--- app.js
+++ app.js
@@ -17,39 +17,53 @@
 
 // Setting up promises
 
-function savetoDB(data, success, failure) {
-  let internetSpeed = Math.floor(Math.random() * 10) + 1;
-  if (internetSpeed > 4) {
-    success();
-  } else {
-    failure();
-  }
+// function savetoDB(data, success, failure) {
+//   let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//   if (internetSpeed > 4) {
+//     success();
+//   } else {
+//     failure();
+//   }
+// }
+
+// savetoDB(
+//   "Athef Ak",
+//   () => {
+//     console.log("Your data has been stored");
+//     savetoDB(
+//       "Hello",
+//       () => {
+//         console.log("Your data 2 has been stored");
+//         savetoDB(
+//           "Hi",
+//           () => {
+//             console.log("Your data 3 has been stored");
+//           },
+//           () => {
+//             console.log("Weak Connection, Data 3 Not stored !");
+//           }
+//         );
+//       },
+//       () => {
+//         console.log("Weak Connection, Data 2 Not stored !");
+//       }
+//     );
+//   },
+//   () => {
+//     console.log("Weak Connection, Data Not stored !");
+//   }
+// );
+
+function savetoDB(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("Data was stored !");
+    } else {
+      reject("Data was not stored");
+    }
+  });
 }
 
-savetoDB(
-  "Athef Ak",
-  () => {
-    console.log("Your data has been stored");
-    savetoDB(
-      "Hello",
-      () => {
-        console.log("Your data 2 has been stored");
-        savetoDB(
-          "Hi",
-          () => {
-            console.log("Your data 3 has been stored");
-          },
-          () => {
-            console.log("Weak Connection, Data 3 Not stored !");
-          }
-        );
-      },
-      () => {
-        console.log("Weak Connection, Data 2 Not stored !");
-      }
-    );
-  },
-  () => {
-    console.log("Weak Connection, Data Not stored !");
-  }
-);
+savetoDB("AthefAk");
+savetoDB("Pandu");
