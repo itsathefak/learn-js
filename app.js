61d67b633f5dd031e3aea03f0506a12b03ca79d9
itsathefak <athefayubkhan@gmail.com>
2024-08-28 15:42:24 -0400

    JSON stringify

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-let jsonRes = '{"Facts" : "Cats are amazing"}';
+// let jsonRes = '{"Facts" : "Cats are amazing"}';
 
-let validRes = JSON.parse(jsonRes);
-console.log(validRes.Facts);
+// let validRes = JSON.parse(jsonRes);
+// console.log(validRes.Facts);
+
+let student = {
+  name: "Athef",
+  marks: 95,
+};
+
+let str = JSON.stringify(student);
+console.log(str);
