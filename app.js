9a7eb64f3173d4ee9892a4128e859658594bbac1
itsathefak <athefayubkhan@gmail.com>
2024-09-09 18:17:53 -0400

    sending headers axios

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-let btn = document.querySelector("button");
 let url2 = "https://dog.ceo/api/breeds/image/random";
 
-btn.addEventListener("click", async () => {
-  let link = await getImg();
-  console.log(link);
-
-  let img = document.querySelector("#output");
-  img.setAttribute("src", link);
-});
-
-async function getImg() {
+async function getNum() {
   try {
+    const config = { Headers: { accept: application / json } };
     let res = await axios(url2);
-    console.log(res.data.message);
-  } catch (e) {
-    console.log("error - ", e);
-    return "No image Found";
+    console.log(res, config);
+  } catch (err) {
+    console.log(err);
   }
 }
-
-// btn.addEventListener("click", async () => {
-//   let res = await getFact();
-//   // console.log(res);
-
-//   let p = document.querySelector("#output");
-//   p.innerText = res;
-// });
-
-// let url = "https://catfact.ninja/fact";
-
-// async function getFact() {
-//   try {
-//     let res = await axios(url);
-//     return res.data.fact;
-//   } catch (e) {
-//     console.log("error - ", e);
-//     return "No Fact Found";
-//   }
-// }
