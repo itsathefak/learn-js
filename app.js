7940cf7af4012f68ea0f4a1d445310c164997cf8
itsathefak <athefayubkhan@gmail.com>
2024-09-09 11:01:12 -0400

    Using Fetch for API

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-// let jsonRes = '{"Facts" : "Cats are amazing"}';
+let url = "https://catfact.ninja/fact";
 
-// let validRes = JSON.parse(jsonRes);
-// console.log(validRes.Facts);
+async function getFact() {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
 
-let student = {
-  name: "Athef",
-  marks: 95,
-};
-
-let str = JSON.stringify(student);
-console.log(str);
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+    console.log(data2);
+  } catch (e) {
+    console.log("error - ", e);
+  }
+  console.log("bye");
+}
